5129637adc63e43000ee2755a4d5cb32a2a694fb
brcadal <brucadal@gmail.com>
2025-06-22 17:21:07 +0700

    attribute, parameter, and Constructor

--- src/main/java/org/demo/Main.java
+++ src/main/java/org/demo/Main.java
@@ -1,7 +1,29 @@
 package org.demo;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    String merk1;
+    String bunyi1;
+    Integer roda1;
+    Integer spion1;
+
+    public Main(String merk, String bunyi, Integer roda, Integer spion){
+        this.bunyi1 = bunyi;
+        this.roda1 = roda;
+        this.spion1 = spion;
+        this.merk1 = merk;
     }
+
+    public void infoHewan() {
+        System.out.println("Merk" + merk1 + "Bunyi" + bunyi1 + "Jumlah Spion" + spion1 + "Jumlah Roda" + roda1);
+    }
+
+    public static void main(String [] param){
+        Main Kucing = new Main("Ambuyan", "meong", 2,2);
+        Main Mio = new Main("Mio", "Ngengg", 2,2);
+
+        Kucing.infoHewan();
+        Mio.infoHewan();
+    }
+
 }
+
